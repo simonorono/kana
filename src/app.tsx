c8871fcde5f56fc06b792f60a67adc51d2d11cb5
Simón Oroño <simonorono@protonmail.com>
2025-05-03 11:10:35 -0400

    Initialize empty project with simple table to display kana

---
+++ src/app.tsx
@@ -0,0 +1,50 @@
+import KANA from "./data.ts";
+import {useState} from "react";
+
+const HIRAGANA = 'hiragana';
+const KATAKANA = 'katakana';
+
+export function App() {
+    const [kana, setKana] = useState(HIRAGANA)
+
+    return (
+        <div>
+            <select onChange={ev => setKana(ev.target.value)}>
+                {[HIRAGANA, KATAKANA].map(opt => (
+                    <option
+                        key={opt}
+                        value={opt}
+                        selected={opt === kana}
+                    >
+                        {opt}
+                    </option>
+                ))}
+            </select>
+
+            <table>
+                <thead>
+                <tr>
+                    <th></th>
+                    <th>a</th>
+                    <th>i</th>
+                    <th>u</th>
+                    <th>e</th>
+                    <th>o</th>
+                </tr>
+                </thead>
+                <tbody>
+                {Object.entries(KANA).map(([key, value]) => (
+                    <tr>
+                        <th>{key}</th>
+                        <td>{value.a[kana]}</td>
+                        <td>{value.i[kana]}</td>
+                        <td>{value.u[kana]}</td>
+                        <td>{value.e[kana]}</td>
+                        <td>{value.o[kana]}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </div>
+    )
+}
---
+++ src/data.ts
@@ -0,0 +1,73 @@
+const KANA = {
+    'a': {
+        'a': {hiragana: 'あ', katakana: 'ア'},
+        'i': {hiragana: 'い', katakana: 'イ'},
+        'u': {hiragana: 'う', katakana: 'ウ'},
+        'e': {hiragana: 'え', katakana: 'エ'},
+        'o': {hiragana: 'お', katakana: 'オ'},
+    },
+    'k': {
+        'a': {hiragana: 'か', katakana: 'カ'},
+        'i': {hiragana: 'き', katakana: 'キ'},
+        'u': {hiragana: 'く', katakana: 'ク'},
+        'e': {hiragana: 'け', katakana: 'ケ'},
+        'o': {hiragana: 'こ', katakana: 'コ'},
+    },
+    's': {
+        'a': {hiragana: 'さ', katakana: 'サ'},
+        'i': {hiragana: 'し', katakana: 'シ'},
+        'u': {hiragana: 'す', katakana: 'ス'},
+        'e': {hiragana: 'せ', katakana: 'セ'},
+        'o': {hiragana: 'そ', katakana: 'ソ'},
+    },
+    't': {
+        'a': {hiragana: 'た', katakana: 'タ'},
+        'i': {hiragana: 'ち', katakana: 'チ'},
+        'u': {hiragana: 'つ', katakana: 'ツ'},
+        'e': {hiragana: 'て', katakana: 'テ'},
+        'o': {hiragana: 'と', katakana: 'ト'},
+    },
+    'n': {
+        'a': {hiragana: 'な', katakana: 'ナ'},
+        'i': {hiragana: 'に', katakana: 'ニ'},
+        'u': {hiragana: 'ぬ', katakana: 'ヌ'},
+        'e': {hiragana: 'ね', katakana: 'ネ'},
+        'o': {hiragana: 'の', katakana: 'ノ'},
+    },
+    'h': {
+        'a': {hiragana: 'は', katakana: 'ハ'},
+        'i': {hiragana: 'ひ', katakana: 'ヒ'},
+        'u': {hiragana: 'ふ', katakana: 'フ'},
+        'e': {hiragana: 'へ', katakana: 'ヘ'},
+        'o': {hiragana: 'ほ', katakana: 'ホ'},
+    },
+    'm': {
+        'a': {hiragana: 'ま', katakana: 'マ'},
+        'i': {hiragana: 'み', katakana: 'ミ'},
+        'u': {hiragana: 'む', katakana: 'ム'},
+        'e': {hiragana: 'め', katakana: 'メ'},
+        'o': {hiragana: 'も', katakana: 'モ'},
+    },
+    'y': {
+        'a': {hiragana: 'や', katakana: 'ヤ'},
+        'i': {hiragana: null, katakana: null},
+        'u': {hiragana: 'ゆ', katakana: 'ユ'},
+        'e': {hiragana: null, katakana: null},
+        'o': {hiragana: 'よ', katakana: 'ヨ'},
+    },
+    'r': {
+        'a': {hiragana: 'ら', katakana: 'ラ'},
+        'i': {hiragana: 'り', katakana: 'リ'},
+        'u': {hiragana: 'る', katakana: 'ル'},
+        'e': {hiragana: 'れ', katakana: 'レ'},
+        'o': {hiragana: 'ろ', katakana: 'ロ'},
+    },
+    'w': {
+        'a': {hiragana: 'わ', katakana: 'ワ'},
+        'i': {hiragana: null, katakana: null},
+        'u': {hiragana: null, katakana: null},
+        'e': {hiragana: null, katakana: null},
+        'o': {hiragana: 'を', katakana: 'ヲ'},
+    },
+}
+export default KANA
