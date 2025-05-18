251eef683d47a826634d5506853c0a9d96e8d2e3
Simón Oroño <simonorono@protonmail.com>
2025-05-17 20:11:38 -0400

    Add key to root element in loop

--- src/app.tsx
+++ src/app.tsx
@@ -18,7 +18,7 @@ export function App() {
     <div>
       <div>
         {[HIRAGANA, KATAKANA].map(v => (
-          <label>
+          <label key={v}>
             <input
               checked={v === kana}
               name="kana"
