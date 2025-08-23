9c87c66f29f22aa9e3ef49eb734dd9a7220f47c0
Simón Oroño <simonorono@protonmail.com>
2025-08-23 18:34:27 -0400

    Create header and page for the game

--- rewrite/app/layouts/default.vue
+++ rewrite/app/layouts/default.vue
@@ -1,5 +1,20 @@
 <template>
-  <div>
+  <header>
+    <div
+      class="flex w-full flex-row items-center justify-between p-2 text-xl shadow shadow-green-700"
+    >
+      <NuxtLink class="text-4xl font-bold" href="/">
+        <span>シ</span><small class="text-sm">(shi)</small>
+      </NuxtLink>
+      <NuxtLink class="block text-blue-800 underline" href="/game">
+        Game
+      </NuxtLink>
+    </div>
+  </header>
+
+  <main>
     <slot />
-  </div>
+  </main>
+
+  <footer class="h-20" />
 </template>
